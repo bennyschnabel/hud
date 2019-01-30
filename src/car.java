ef204300f53fa59ac7be115915c95020469d3e9e
Benjamin <bennyschnabel@me.com>
2019-01-30 16:14:59 +0100

    Create car.java

---
+++ src/car.java
@@ -0,0 +1,19 @@
+
+public class car {
+	
+	private float length;
+	private float width;
+	
+	public int currentSpeed() {
+		return 60;
+	}
+	
+	public float getLength() {
+		return length;
+	}
+	
+	public float getWidth() {
+		return width;
+	}
+
+}
